8712fefdde6d033447c6d909b6764a5803aeffe4
unknown <chitrayadav190@gmail.com>
2020-11-20 17:35:28 +0530

    Based on this data, its existence should be checked here https://vpic.nhtsa.dot.gov/api/

--- cars/views.py
+++ cars/views.py
@@ -2,11 +2,50 @@ from django.shortcuts import render
 from rest_framework import generics
 from cars.serializers import CarSerializer
 from core.models import Car
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from django.http import HttpRequest
+from rest_framework import status
+from core.models import Car
+from cars.serializers import CarSerializer
+from django_filters.rest_framework import DjangoFilterBackend
+import requests
+import json
+from django.http import HttpResponse
+from rest_framework import filters
+
+
 # Create your views here.
 
 
 class createCarView(generics.ListCreateAPIView):
     """Create a new car in the system"""
-    model= Car
-    serializer_class=CarSerializer
+    model = Car
+    serializer_class = CarSerializer
     queryset = Car.objects.all()
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['make_name', 'model_name']
+
+    def post(self, request):
+        global dict_json
+        serializer = CarSerializer(data=request.data)
+        # r=requests.post(url,serializer)
+        url = 'https://vpic.nhtsa.dot.gov/api/vehicles/getmodelsformake/honda?format=json'
+        r = requests.get(url)
+        url_data = r.json()
+        Data_str = json.dumps(url_data)
+        if (request.method == 'POST'):
+            json_data = json.dumps(request.data)
+            dict_json = json.loads(json_data)
+            for i in range(0, 504):
+                if url_data['Results'][i]['Model_ID'] == int(dict_json['model_id']):
+                    serializer=CarSerializer(data=request.data)
+                    if serializer.is_valid():
+                        serializer.save()
+                    return Response(int(dict_json['model_id']), status=status.HTTP_201_CREATED)
+            return Response("Not found Cannot add the data to the database", status=status.HTTP_400_BAD_REQUEST)
+            # make_name=request.POST['make_name']
+            # model_name=serializer.data.get('Model_name')
+            # PARAMS={'Make_name':make_name, 'Model_name':model_name}
+            # r=request.post(url=url, json={"make_name":make_name, "model_name": model_name})
+            # return Response("Not found", status=status.HTTP_201_CREATED)
--- core/models.py
+++ core/models.py
@@ -4,20 +4,20 @@ from django.db import models
 
 #docker-compose run app sh -c ""
 class Car(models.Model):
-    make_id=models.CharField(max_length=24, unique=False)
+    make_id=models.IntegerField(max_length=24, unique=False)
     make_name=models.CharField(max_length=100, unique=False)
-    model_id=models.CharField(max_length=25,unique=True,primary_key=True)
+    model_id=models.IntegerField(max_length=25,unique=True,primary_key=True)
     model_name=models.CharField(max_length=100, unique=True)
 
     def __str__(self):
         return self.make_id.make_name.model_id.model_name
-# class Rating(models.Model):
-#     rating=((1,'Poor'),(2,'Below Average'),(3,'Average'),(4,'Good'),(5,'Excellent'))
-#     car=models.ForeignKey('Car',on_delete=models.CASCADE)
-#     car_rating=models.IntegerField(choices=rating)
-#
-#     def __str__(self):
-#         return self.car_rating
+class Rating(models.Model):
+    rating=((1,'Poor'),(2,'Below Average'),(3,'Average'),(4,'Good'),(5,'Excellent'))
+    car=models.ForeignKey('Car',on_delete=models.CASCADE)
+    car_rating=models.IntegerField(choices=rating)
+
+    def __str__(self):
+        return self.car_rating
 #
 # class AccessRecord(models.Model):
 #     car_record=models.ForeignKey('Rating',on_delete=models.CASCADE)
